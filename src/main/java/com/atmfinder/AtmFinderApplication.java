98fe0fb29baf0abe449919749c5505917ffb417a
Your Name <you@example.com>
2021-03-11 23:38:21 +0530

    adding project to repo

---
+++ src/main/java/com/atmfinder/AtmFinderApplication.java
@@ -0,0 +1,13 @@
+package com.atmfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atmfinder/AtmFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
